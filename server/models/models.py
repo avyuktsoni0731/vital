49fec9db22327531ba62bf6e471ae1ea3a36269f
Avyukt Soni <soniavyukt@gmail.com>
2024-05-22 23:19:07 +0530

    Update models.py

--- server/models/models.py
+++ server/models/models.py
@@ -13,6 +13,7 @@ generation_config = {
   "top_p": 1,
   "top_k": 1,
   "max_output_tokens": 2048,
+  "response_mime_type": "text/plain",
 }
 
 safety_settings = [
@@ -34,11 +35,11 @@ safety_settings = [
   },
 ]
 
-model = genai.GenerativeModel(model_name="gemini-1.0-pro",
+model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",
                               generation_config=generation_config,
                               safety_settings=safety_settings)
 
 convo = model.start_chat()
 
-# convo.send_message("i am 17 year old. i am a male. i have allergies from peanuts, cheese, olive oil. i have stomachache too frequently. how can i improve it? give me prompt in the format, 1st->allergies, 2nd->things to avoid that can trigger, 3rd->medicinal supplements that one can have related to it, 4->seeking professional help. make sure prompt is strictly in this format. i want a description regarding them as well, along each point. dont mention description separately, give more information about everything in the same point itself.")
-# print(convo.last.text)
+convo.send_message("i am 17 year old. i am a male. i have allergies from peanuts, cheese, olive oil. i have stomachache too frequently. how can i improve it? give me prompt in the format, 1st->allergies, 2nd->things to avoid that can trigger, 3rd->medicinal supplements that one can have related to it, 4->seeking professional help. make sure prompt is strictly in this format. i want a description regarding them as well, along each point. dont mention description separately, give more information about everything in the same point itself.")
+print(convo.last.text)
